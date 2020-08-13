39440e3166a179947105cb3bddd879c4dc32e2cf
KritiPatil <66488628+KritiPatil@users.noreply.github.com>
2020-08-13 10:50:19 -0700

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -4,8 +4,8 @@ var dog, goodDog, happyDog, database, foodS ,foodStock;
 function preload()
 {
   //load images here
-  goodDog = loadImage("dogImg.png");
-  happyDog = loadImage("dogImg1.png");
+  goodDog = loadImage("images/dogImg.png");
+  happyDog = loadImage("images/dogImg1.png");
 }
 
 function setup() {
@@ -14,8 +14,8 @@ function setup() {
   createCanvas(500, 500);
   
   imageMode(CENTER);
-  image(goodDog, 100, 250, 0.5, 0.5);
-  image(happyDog, 400, 250, 200, 200);
+  //image(goodDog, 100, 250, 0.5, 0.5);
+  //image(happyDog, 400, 250, 200, 200);
 
   dog = createSprite(250, 250, 20, 20);
   dog.scale = 0.25;
@@ -33,16 +33,18 @@ background (46, 139, 87)
   if(keyWentDown (UP_ARROW)) {
       writeStock(foodS);
       dog.addImage(happyDog);
-      
+  }
+  
+  if(keyWentUp (UP_ARROW)) {
+    dog.addImage(goodDog);
   }
 
   drawSprites();
   //add styles here
-  textSize(30);
-  fill ("black");
+  textSize(20);
+  textFont('Georgia');
   stroke(4);
-  text("foodStock",+ foodS, 450, 10);
-  text ("Press up key to feed the dog.", 450, 20);
+  text("foodStock : ", 370, 20);
 
 }
 
